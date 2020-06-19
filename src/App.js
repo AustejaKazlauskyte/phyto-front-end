3c780c2dc52dee2bb4e01c16637e60effb1e25a2
Austeja Kazlauskyte <akazlauskyte@gmail.com>
2020-06-19 12:09:33 +0300

    Added React Route

--- src/App.js
+++ src/App.js
@@ -3,17 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 import Login from "./Login";
 import Menu from "./Menu";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import HerbDatabase from "./HerbDatabase";
+import About from "./About";
 
-class App extends React.Component {
-    render() {
-        return (
+export default function App() {
+    return (
+        <Router>
             <div>
-                <Menu/>
-                <Login/>
-            </div>
-        )
-    } ;
+                <nav>
+                    <ul className="menu-display">
+                        <li className="menu-item">
+                            <Link to="/">Prisijungti</Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/about">Apie Phyto</Link>
+                        </li>
+                        <li className="menu-item">
+                            <Link to="/herbdatabase">Vaistažolių duomenų bazė</Link>
+                        </li>
+                    </ul>
+                </nav>
 
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/about">
+                        <About/>
+                    </Route>
+                    <Route path="/herbdatabase">
+                        <HerbDatabase/>
+                    </Route>
+                    <Route path="/">
+                        <Login/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
-export default App;
---
+++ src/HerbDatabase.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+function HerbDatabase() {
+    return (
+        <div>
+          <h3>Čia tuoj bus duomenys</h3>
+
+        </div>
+    )
+}
+
+export default HerbDatabase;
