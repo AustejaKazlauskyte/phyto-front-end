15246658b279c8903f622c2343771da2e225b52d
Austeja Kazlauskyte <akazlauskyte@gmail.com>
2020-07-08 15:53:51 +0300

    Added more field to herb

--- src/forms/AddHerbForm.jsx
+++ src/forms/AddHerbForm.jsx
@@ -2,7 +2,12 @@ import React, {useState} from 'react';
 
 const AddHerbForm = (props) => {
 
-    const initHerb = {id: null, name: ''};
+    const initHerb = {
+        id: null,
+        name: '',
+        characteristics: {id: null, title: '', hasHighBloodPressure: '', child:'', pregnant: ''},
+        medicalConditions: {id: null, title: ''}
+    };
 
     const [herb, setHerb] = useState(initHerb);
 
@@ -18,8 +23,18 @@ const AddHerbForm = (props) => {
 
     return (
         <form>
-            <label>Pavadinimas</label>
+            <label>Vaistažolės pavadinimas</label>
             <input className="u-full-width" type="text" name="name" value={herb.name} onChange={handleChange}/>
+            <label>Charakteristikos</label>
+            <input className="u-full-width" type="text" name="characteristics" value={herb.characteristics.title}/>
+            <label>Ar tinka turinčiam aukštą spaudimą?</label>
+            <input className="u-full-width" type="checkbox" name="hasHighBloodPressure" value={herb.characteristics.hasHighBloodPressure}/>
+            <label>Ar tinka vaikams?</label>
+            <input className="u-full-width" type="checkbox" name="child" value={herb.characteristics.child}/>
+            <label>Ar tinka neščiosioms?</label>
+            <input className="u-full-width" type="checkbox" name="pregnant" value={herb.characteristics.pregnant}/>
+            <label>Negalavimai</label>
+            <input className="u-full-width" type="text" name="medicalConditions" value={herb.medicalConditions.title}/>
             <button className="button-primary" type="submit" onClick={handleSubmit}>Pridėti vaistažolę</button>
         </form>
     )
