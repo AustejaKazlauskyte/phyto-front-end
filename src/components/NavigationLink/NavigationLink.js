c39bc99c130701b4ccb35a1d53b34110b4e45b7c
Rutenis Turcinas <rutenis.turcinas@devbridge.com>
2020-07-18 21:41:19 +0300

    Navigation

--- src/components/NavigationLink/NavigationLink.js
+++ src/components/NavigationLink/NavigationLink.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import './NavigationLink.css';
 
 export default ({href, text}) => (
     <li className="navbar-item">
-        <a className="navbar-link" href={href}>{text}</a>
+        <NavLink
+            className="navbar-link"
+            to={href}
+        >
+            {text}
+        </NavLink>
     </li>
 );
