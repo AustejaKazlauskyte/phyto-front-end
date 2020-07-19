d09b5783080decca274726f7a96424ee7890537f
Austeja Kazlauskyte <akazlauskyte@gmail.com>
2020-07-19 12:23:03 +0300

    Data fetch function not working

--- src/tables/ExtendedHerbTable.jsx
+++ src/tables/ExtendedHerbTable.jsx
@@ -13,35 +13,35 @@ const ExtendedHerbTable = (props) => {
                 <th>Ar tinka vaikams?</th>
                 <th>Ar tinka nėščiosioms?</th>
                 <th>Negalavimai</th>
-                <th>Veiksmai</th>
+               {/* <th>Veiksmai</th>*/}
             </tr>
             </thead>
             <tbody>
             {props.herbs.length > 0 ? (
                 props.herbs.map(herb => {
                     const {id, name, characteristics, medicalConditions} = herb;
-                    console.log(characteristics);
+                    console.log(characteristics[0]);
                     return (
                         <tr key={id}>
                             <td>{id}</td>
                             <td>{name}</td>
                             <td>{characteristics[0].title}</td>
-                            <td>{characteristics[0].hasHighBloodPressure === "checked" ? "TAIP" : "NE"}</td>
-                            <td>{characteristics[0].child === "checked" ? "TAIP" : "NE"}</td>
-                            <td>{characteristics[0].pregnant === "checked" ? "TAIP" : "NE"}</td>
+                            <td>{characteristics[0].hasHighBloodPressure ? "TAIP" : "NE"}</td>
+                            <td>{characteristics[0].child ? "TAIP" : "NE"}</td>
+                            <td>{characteristics[0].pregnant ? "TAIP" : "NE"}</td>
                             <td>{medicalConditions[0].title}</td>
-                            <td>
+                          {/*  <td>
 
                                 <i onClick={() => props.viewExtendedHerbs(herb)} className="far fa-eye"></i>
                                 <i onClick={() => props.editHerb(id, herb)} className="far fa-edit"></i>
                                 <i onClick={() => props.deleteHerb(id)} className=" fas fa-trash"></i>
-                            </td>
+                            </td>*/}
                         </tr>
                     )
                 })
             ) : (
                 <tr>
-                    <td colSpan={8}>Vaistažolių sąrašas tuščias</td>
+                    <td colSpan={7}>Vaistažolių sąrašas tuščias</td>
                 </tr>
             )
             }
