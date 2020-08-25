880d522eadcc1a5207a67c9cbb8ae6d7ac7531b7
Austeja Kazlauskyte <akazlauskyte@gmail.com>
2020-08-26 00:50:18 +0300

    Husky

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Text from './Text';
 const App = (): JSX.Element => (
     <div className="App">
         <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
             <p>
                 Edit <code>src/App.tsx</code> and save meh <Text />.
             </p>
